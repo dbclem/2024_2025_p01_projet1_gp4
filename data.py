40d065e42a8fa9b6c8042e318fe9baadeee6ea15
clemdeboisfleury <deboisfleuryclement@gmail.com>
2024-09-27 12:01:00 +0200

    création des requêtes

--- data.py
+++ data.py
@@ -1,2 +1,6 @@
 from main import *
 from tools import * 
+
+ask_for_init_number_text = "Entrez le numbre que vous voulez transformer : "
+ask_for_init_base_text = "Entrez la base que vous voulez utiliser : "
+ask_for_target_base_text = "Entrez la base qeu vous voulez viser : "
--- main.py
+++ main.py
@@ -14,3 +14,96 @@ def do_the_job ():
 
 
 assert bin_dec_hex_to_bin_dec_hex("101", 2, 10) == "5"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def ask_for_the_init_number() : 
+    return input(ask_for_init_number_text)
+
+def ask_for_the_init_base() : 
+    return input(ask_for_init_base_text)
+
+
+def ask_for_the_target_base() :
+    return input(ask_for_target_base_text)
+
