04adfa9ebc3859f29279fe914e0f1f0f8bdb6450
Roman-milos <romanmilos8@gmail.com>
2024-09-30 15:21:09 +0200

    update dec_to_bin

--- roman.py
+++ roman.py
@@ -5,7 +5,6 @@ def dec_to_bin (init_number) :
     bin_number = 0
     while bin_number != 0:
         bin_number = number(init_number) + bin_number
-        print (bin_number)
     return bin_number
 
 print (dec_to_bin(91))
