eb43987d34769b32d24a33e3829029eea0137e2b
Roman-milos <romanmilos8@gmail.com>
2024-10-04 11:54:45 +0200

    dec_to_bin
    progression de la def dec_to_bin

--- roman.py
+++ roman.py
@@ -1,14 +1,16 @@
-def number (n):
+from math import*
+
+def numbers_left (n):
     return n % 2
+
 
 def dec_to_bin (init_number) :
     bin_number = 0
-    while bin_number != 0:
-        bin_number = number(init_number) + bin_number
+    while init_number > 0:
+        bin_number = numbers_left(init_number) + bin_number
+        init_number = init_number // 2
     return bin_number
 
 print (dec_to_bin(91))
 
-
-
-
+print (numbers_left(9))
