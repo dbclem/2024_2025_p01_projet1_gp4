58622fb2164ff40788663de2c189cbfb94f6d08e
Roman-milos <romanmilos8@gmail.com>
2024-10-04 12:05:41 +0200

    dec_to_bin
    finalisation de la def dec_to_bin

--- roman.py
+++ roman.py
@@ -5,12 +5,11 @@ def numbers_left (n):
 
 
 def dec_to_bin (init_number) :
-    bin_number = 0
+    bin_number = '0'
     while init_number > 0:
-        bin_number = numbers_left(init_number) + bin_number
+        bin_number = str(numbers_left(init_number)) + bin_number
         init_number = init_number // 2
     return bin_number
 
 print (dec_to_bin(91))
 
-print (numbers_left(9))
