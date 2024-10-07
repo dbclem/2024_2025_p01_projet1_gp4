50c4f680eeb32717b8179ef1fe12cb2b6509ae4a
Roman-milos <romanmilos8@gmail.com>
2024-10-07 15:20:47 +0200

    bin_to_dec
    commencement bin_to_dec

--- roman.py
+++ roman.py
@@ -4,20 +4,32 @@ def rest_number (n):
     return n % 2
 
 
-def dec_to_bin (init_number) :
-    bin_number = '0'
-    while init_number > 0:
-        bin_number = str(rest_number(init_number)) + bin_number
-        init_number = init_number // 2
-    return bin_number
+# def dec_to_bin (init_number) :
+#     bin_number = '0'
+#     while init_number > 0:
+#         bin_number = str(rest_number(init_number)) + bin_number
+#         init_number = init_number // 2
+#     return bin_number
 
-print (dec_to_bin(17))
+# print (dec_to_bin(17))
 
 
-def bin_to_dec (n, b):
-    dec_number = 0
-    for i in range (len(n)):
-        dec_number =  dec_number + n[ len(n) - i - 1 ] * b ** i
-    return dec_number
+# def bin_to_dec (n, b):
+#     dec_number = 0
+#     for i in range (len(n)):
+#         dec_number =  dec_number + n[ len(n) - i - 1 ] * b ** i
+#     return dec_number
 
-print (bin_to_dec (10100, 2))
+# print (bin_to_dec (10100, 2))
+
+# binaire = "1010"
+# decimal = 0
+# puissance = len(binaire) - 1
+
+# for chiffre in binaire:
+#     if chiffre == "1":
+#         decimal += 2**puissance
+#     puissance -= 1
+# print (decimal)
+
+
