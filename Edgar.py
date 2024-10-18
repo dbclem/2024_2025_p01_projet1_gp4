9cf4d851c58b64afacb50efd6f846e933d14506f
clemdeboisfleury <deboisfleuryclement@gmail.com>
2024-10-18 11:52:14 +0200

    clement

--- Edgar.py
+++ Edgar.py
@@ -1,16 +1,1 @@
-from math import*
-
-
-def dec_to_hex (init_number):
-    hex_number = ""
-    hex_chars = "0123456789ABCDEF"
-
-    while init_number > 0:
-        point_hex_char = init_number % 16
-        hex_number = hex_chars[point_hex_char] + hex_number
-        init_number = init_number // 16
-    return hex_number
-
-print (dec_to_hex(91))
-
 
